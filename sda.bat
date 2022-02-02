c4f6d9a026baa53e91bc572c75ca197c0d751446
skipper3394 <86693561+skipper3394@users.noreply.github.com>
2022-02-02 09:47:29 +0100

    Add files via upload

---
+++ sda.bat
@@ -0,0 +1,7 @@
+echo "# mapa-odsylaczy" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/skipper3394/mapa-odsylaczy.git
+git push -u origin mai
